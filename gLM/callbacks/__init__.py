a85a9c5784b08f0590580978860e60d279a414f0
Sinha <as12267@bigpurple-ln3.cm.cluster>
2026-06-03 14:02:25 -0400

    Clean up PhylogeneticTraining repo

--- gLM/callbacks/__init__.py
+++ gLM/callbacks/__init__.py
@@ -1,7 +1,5 @@
 from .variant_effect import ZeroShotVEPEvaluationCallback
-from .percent_identity import PercentIdentityLoggingCallback
 
 __all__ = [
     "ZeroShotVEPEvaluationCallback",
-    "PercentIdentityLoggingCallback"
 ]
